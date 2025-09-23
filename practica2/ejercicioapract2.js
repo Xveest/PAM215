924ff619e001d44b5b90966210a197510d8555b7
Xveest <diegoespisalinas@gmail.com>
2025-09-22 23:10:53 -0600

    Create ejercicioapract2.js
    primer ejercicio de la segunda practica implementando la destructuracion para las variables

---
+++ practica2/ejercicioapract2.js
@@ -0,0 +1,16 @@
+const persona = {
+    nombre: "Diego",
+    edad: 19,
+    direccion: {
+        ciudad: "Qro",
+        pais: "MX"
+    }
+};
+
+
+const { nombre, edad, direccion: { ciudad } } = persona;
+
+console.log("Me llamo " + nombre + ", tengo " + edad + " años y vivo en " + ciudad + ".");
+
+
+
