bc7d070755d55c9c074d73da44ee6ffc551748d6
Xveest <diegoespisalinas@gmail.com>
2025-10-10 20:11:11 -0600

    Creacion de componentes
    Creacion de componentes en react en especifico 3 botones de un contador los cuales son incrementa decrementa y reinicia

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,21 @@
+// zona 1 imports: zona de importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,Button } from 'react-native';
+import React,{useState} from 'react';
+// zona 2 Main: zona de componentes
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Hola mundo</Text>
+      <Text>Contador: {count}</Text>
+      <Button title= 'incrementar' onPress={() => setCount(count + 1)}></Button>
+      <Button title= 'decrementar' onPress={() => setCount(count - 1)}></Button>
+      <Button title= 'reiniciar' onPress={() => setCount(0)}></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+//zona 3 styles: zona de estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
