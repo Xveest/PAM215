d074b48b3ced69941e0aae8cb1570a46f014c555
Xveest <diegoespisalinas@gmail.com>
2025-11-07 17:16:18 -0600

    Carpeta examen

--- intro/screens/ActivityScreen.js
+++ intro/screens/ActivityScreen.js
@@ -1,13 +1,57 @@
-import { Text, StyleSheet, View } from 'react-native'
-import React, { Component } from 'react'
+import { Text, StyleSheet, View, Button, ActivityIndicator } from "react-native"
+import React, {useState} from "react";
 
-export default function BotonesScreen() {
-    
-  return (
-    <View>
-      <Text>Proximamente por Daniel jr </Text>
-    </View>
-  )
+export default function ActScreen() {
+    const [loading, setLoading] = useState(false);
+    const startLoading = () => {
+        setLoading(true);
+        setTimeout(() => setLoading(false), 3000)
+    };
+
+    if (loading) {
+        return (
+        <View style={styles.container}>
+            <View style={styles.loadingContainer}>
+                <ActivityIndicator
+                    size="large"
+                    color="#1212e7ff"
+                    animating={true}
+                    hidesWhenStopped={true}
+                />
+
+                <Text color= '#9f90e0ff'>Cargando...</Text>
+            </View>
+        </View>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text> ActivityIndicator </Text>
+            <Button title="Carga de datos" onPress={startLoading} />
+        </View>
+    )
+
 }
-   
-const styles = StyleSheet.create({})
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#5d2e2eff',
+    },
+    texto:{
+        color: '#9f90e0ff',
+    },
+    title:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },      
+    loadingContainer:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
--- intro/screens/FlatScreen.js
+++ intro/screens/FlatScreen.js
@@ -1,12 +1,54 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, FlatList,SectionList, ScrollView } from 'react-native'
 import React, { Component } from 'react'
 
-export default function BotonesScreen() {
+export default function FlatScreen() {
+  
+  const datos = [
+    { id: '1', nombre: 'manzana'  },
+    { id: '2', nombre: 'platano'  },
+    { id: '3', nombre: 'naranja'  },
+    { id: '4', nombre: 'uva'  },
+    { id: '5', nombre: 'fresa'  },
+    { id: '6', nombre: 'sandia'  },
+  ]
+const secciones = [
+  {
+    titulo: 'frutas',
+    data:[
+      {nombre: 'manzana'},
+      {nombre: 'platano'},
+      {nombre: 'naranja'},
+      {nombre: 'uva'},
     
+
+    ]
+  },
+  {
+    titulo: 'verduras',
+    data: [
+      {nombre: 'zanahoria'},
+      {nombre: 'lechuga'},
+      {nombre: 'tomate'},
+      {nombre: 'brocoli'},
+
+    ]
+  },
+]
+
   return (
-    <View>
-      <Text>Proximamente por Daniel jr </Text>
-    </View>
+    <ScrollView style={styles.container} >
+      <View style={styles.content} >
+        <Text style={styles.titulo}> Ejemplo de FlatList</Text>
+
+        <FlatList
+        
+        
+        
+        > </FlatList>
+        </View>
+      
+      
+       </ScrollView>
   )
 }
    
